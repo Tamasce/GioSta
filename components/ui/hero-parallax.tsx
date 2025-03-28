8f773af286a17037caadf94ebebfa363cd69cd79
Tamasce <giovannistancampiano1999@gmail.com>
2025-03-28 15:41:13 +0100

    Update hero-parallax.tsx

--- components/ui/hero-parallax.tsx
+++ components/ui/hero-parallax.tsx
@@ -311,9 +311,10 @@ export const ProductCard = ({
         />
       
       <div className="absolute inset-0 h-full w-full opacity-0 group-hover/product:opacity-80 bg-black pointer-events-none"></div>
-      <h2 className="absolute bottom-4 left-4 opacity-1 group-hover/product:opacity-100 text-white">
-        {product.title}
-      </h2>
+      <h2 className="absolute bottom-4 left-1/2 -translate-x-1/2 opacity-1 group-hover/product:opacity-100 text-white text-center">
+  {product.title}
+</h2>
+
       
     </motion.div>
   );
